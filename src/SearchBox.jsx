3a85fe763a3fcbc1f45f9097f06b5efe09213c41
om mhaske <ommhaske1224@gmail.com>
2024-08-09 22:15:08 +0530

    Moved API key to .env file

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -7,7 +7,7 @@ export default function SearchBox({ updateInfo }) {
     let [city, setCity] = useState("");
     let [error, setError] = useState(false);
     const API_URL = "https://api.openweathermap.org/data/2.5/weather";
-    const API_KEY = "80de11f63c3b43e94075a25956b26543";
+    const API_KEY =process.env.REACT_APP_WEATHER_API;
 
     let getWeatherInfo = async () => {
         try {
